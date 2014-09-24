a7e4a656d4d0a04461468e607fe0e3878fac3f24
seetpal <Creators.guru@gmail.com>
2014-09-24 07:22:11 +0530

    routing continue

--- js/apps.js
+++ js/apps.js
@@ -2,3 +2,29 @@
  * Created by demo on 19-Sep-14.
  */
 
+var app =   angular.module('firstApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+
+    $routeProvider.
+        when('/', {templateUrl: 'views/list.html'}).
+        otherwise(
+            {redirectTo: '/'}
+        )
+}]);
+
+app.controller('listCtrl',  function($scope){
+    $scope.subject  =   [
+        {name:'mobile', property:'calling'},
+        {name:'mouse', property:'pointer'},
+        {name:'keyboard', property:'typing'},
+        {name:'screen', property:'view'}
+    ]
+});
+
+app.controller('editCtrl', function ($scope, $routeParams) {
+
+})
+
+
+
